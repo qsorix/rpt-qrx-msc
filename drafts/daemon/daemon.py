adb0394f965511a1e128cd7f66d2e3352ae42b23
rpt <rpt@8b0f0422-2b3e-45da-bae5-22c1ac97dc01>
2010-01-19 00:01:33 +0000

    Update daemon.py
    git-svn-id: http://svn2.assembla.com/svn/rpt-qrx-msc@14 8b0f0422-2b3e-45da-bae5-22c1ac97dc01

--- drafts/daemon/daemon.py
+++ drafts/daemon/daemon.py
@@ -3,40 +3,55 @@
 import sys
 import os
 import re
-from time import strptime
+from time import strptime, strftime
 
 tests = []
 
 class Test:
-	def __init__(self):
+	def __init__(self, id):
+		self.id = id
 		self.tasks = []
 		self.duration = 0
+		self.start = None
 		self.files = {}
+
+	def __str__(self):
+		output = 'Test ' + str(self.id) + '\n'
+		output += ' Duration: ' + str(self.duration) + '\n'
+		output += ' Start: ' + strftime('%Y-%m-%d %H:%M:%S', self.start) + '\n'
+		for task in self.tasks:
+			output += ' ' + str(task) + '\n'
+		for file in self.files.keys():
+			output += ' File \"' + file + '\": ' + self.files.get(file)[:30] + '\n'
+		return output
 	
 class Task:
 	def __init__(self, start, cmd):
 		self.start = start
 		self.cmd = cmd
 
-line = sys.stdin.readline()
+	def __str__(self):
+		return 'At ' + str(self.start) + ' start: ' + self.cmd
 
-while not re.search('^test [0-9]+$', line.strip()):
+line = sys.stdin.readline().strip()
+
+while not re.search('^test [0-9]+$', line):
 	print '400: Bad Request'
-	line = sys.stdin.readline()
+	line = sys.stdin.readline().strip()
 
 print '200: OK'
-test = Test()
+test = Test(int(line[5:]))
 
 line = sys.stdin.readline().strip()
 while not re.search('^end$', line):
 
 	if re.search('^file \{[a-zA-Z0-9\.\-]+\} [0-9]+$', line):
-		test.files[line.split(' ')[1]] = sys.stdin.readline().strip()
+		test.files[line.split(' ')[1][1:-1]] = sys.stdin.readline().strip()
 		print '200: OK'
 	elif re.search('^schedule [0-9]+$', line):
 		for i in range(0, int(line.strip().split(' ')[1])):
-			task = sys.stdin.readline().strip().split(' ')
-			test.tasks.append(Task(int(task[0][:-1]), task[1]))
+			task = sys.stdin.readline().strip().split(':')
+			test.tasks.append(Task(int(task[0]), task[1][1:]))
 		print '200: OK'
 	elif re.search('^start [0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$', line):
 		test.start = strptime(line[6:], "%Y-%m-%d %H:%M:%S")
@@ -50,3 +65,5 @@ while not re.search('^end$', line):
 	line = sys.stdin.readline().strip()
 
 print '200: OK'
+
+print test
