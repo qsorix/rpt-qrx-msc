c3f8c125807201eb9d314858348219ec7cc9b9e8
Tomasz Rydzyński <qsorix@rydznet.pl>
2010-07-03 22:13:57 +0200

    filenames from cmd line and some clean up

--- configuration/src/Main.py
+++ configuration/src/Main.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python
+
 from config import Configuration
 from command import Executer
+import sys
 
 def dump_configuration(c):
     for h in c.hosts():
@@ -25,14 +28,18 @@ def dump_configuration(c):
 
         print
 
-c = Configuration.Configuration()
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print "run with %s <model> <network> <mapping> <schedule>" % sys.argv[0]
+        sys.exit(1)
 
-c.read('test_model', 'test_network', 'test_mapping', 'test_schedule')
+    c = Configuration.Configuration()
+    c.read(*sys.argv[1:5])
 
-e = Executer.Executer()
+    e = Executer.Executer()
 
-for h in c.hosts():
-    print h.model.name()
-    c = e.generate(h)
-    c.dump()
+    for h in c.hosts():
+        print h.model.name()
+        c = e.generate(h)
+        c.dump()
 
