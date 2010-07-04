8d15f2ee4f6b28da5cba41ba00f95abe29c2a05f
Tomasz Rydzyński <qsorix@rydznet.pl>
2010-07-04 12:41:43 +0200

    sanity checks

--- configuration/src/config/Mapping.py
+++ configuration/src/config/Mapping.py
@@ -2,16 +2,22 @@
 
 class Mapping:
     def __init__(self):
-        self.__bindings = []
+        self.__bindings = set()
 
     def bind(self, first, second):
         first.bind(second)
         second.bind(first)
 
-        b = sorted([first, second])
-        self.bindings().append(b)
+        b = tuple(sorted([first, second]))
+        self.bindings().add(b)
 
     def bindings(self): return self.__bindings
+
+    def clear(self):
+        for (a, b) in self.__bindings:
+            a.unbind()
+            b.unbind()
+        self.__bindings = set()
 
 mapping = Mapping()
 bind = mapping.bind
