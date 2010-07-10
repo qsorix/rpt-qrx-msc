e9c47e3a1f626876c4f58a9a52b7ad7a2a248536
Tomasz Rydzyński <qsorix@rydznet.pl>
2010-07-10 13:11:44 +0200

    TCPConnection plugin

---
+++ configuration/src/plugins/connections/TCPConnection.py
@@ -0,0 +1,28 @@
+from controller.ConnectionPlugin import ConnectionPlugin
+
+import socket
+
+class TCPConnection(ConnectionPlugin):
+    connection_type = 'tcp'
+
+    def __init__(self, host):
+        self._host = host
+        ip = host.network.attributes()['ip']
+        port = host.network.attributes()['connection_port']
+
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.connect((ip, int(port)))
+        self._in  = self._socket.makefile('r', 0)
+        self._out = self._socket.makefile('w', 0)
+
+    def close(self):
+        self._out.close()
+        self._in.close()
+        self._socket.close()
+
+    def input(self):
+        return self._in
+
+    def output(self):
+        return self._out
+
