60590e5ee344ea63bf9ee74c00495bb045c6da27
Tomasz Rydzyński <qsorix@rydznet.pl>
2010-07-26 10:34:18 +0200

    use waf to compile the thesis
    Moved from shell script to Waf and corrected .gitignore as some
    generated files were introduced and others has been removed.

--- thesis/diagrams/generate-postscript.sh
+++
@@ -1,6 +0,0 @@
-FILTER='--filter=eps-builtin'
-EXT=eps
-
-dia konfiguracja-model-lab-mapping.dia ${FILTER} --show-layers=model --export=konfiguracja-model.${EXT}
-dia konfiguracja-model-lab-mapping.dia ${FILTER} --show-layers=laboratory --export=konfiguracja-laboratory.${EXT}
-dia konfiguracja-model-lab-mapping.dia ${FILTER} --show-layers=model,laboratory,mapping --export=konfiguracja-mapping.${EXT}
