40bb70baa2d99b22a44a1954a08b6724cadaed1e
Krzysztof Rutka <rptmail@gmail.com>
2010-08-03 12:22:12 +0200

    Some more changes to the modules structure.

---
+++ daemon/Main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import SocketServer
+import daemon.Daemon as d
+
+if __name__ == "__main__":
+    d.setup_database()
+    d.setup_config()
+
+    HOST, PORT = "localhost", 9999
+    SocketServer.TCPServer.allow_reuse_address = True
+    daemon = SocketServer.TCPServer((HOST, PORT), d.DaemonHandler)
+    daemon.serve_forever()
+
---
+++ daemon/common/Exceptions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+class DaemonError(Exception):
+    def __init__(self, exception):
+        Exception.__init__(self, exception)
+
+class CheckError(DaemonError):
+    pass
+
+class LineError(DaemonError):
+    pass
+
+class ParentError(DaemonError):
+    pass
+
+class TypeError(DaemonError):
+    pass
+
+class ParamError(DaemonError):
+    pass
+
+class ValueError(DaemonError):
+    pass
+
+class DatabaseError(Exception):
+    pass
+
