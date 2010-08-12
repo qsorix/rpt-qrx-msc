00a92c182d66dcc6297b39b48c54b9d55f5db0a0
Krzysztof Rutka <rutka@student.agh.edu.pl>
2010-08-12 14:33:30 +0200

    Added 'Every'. Some changes you made when you were here.

--- controller/utils/schedule.py
+++ controller/utils/schedule.py
@@ -8,9 +8,19 @@ class At(Schedule.RunPolicy):
 
     def schedule_for_daemon(self):
         return 'at %i' % self._time
+
+class Every(Schedule.RunPolicy):
+    def __init__(self, time):
+        self._time = time
+
+    def schedule_for_daemon(self):
+        return 'every %i' % self._time
     
 def at(time):
     return At(time)
+
+def every(time):
+    return Every(time)
 
 class ShellCommand(Schedule.Command):
     def __init__(self, command, resources):
