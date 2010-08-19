eb96a74d612efcbbb6f0166f29cba1075cad4411
Krzysztof Rutka <rutka@student.agh.edu.pl>
2010-08-19 20:43:11 +0200

    Have to work on those command line arguments. Disabled for now.

--- daemon/Main.py
+++ daemon/Main.py
@@ -7,5 +7,5 @@ from modules.Daemon import Daemon
 
 if __name__ == "__main__":
     # TODO Add some command line parameters
-    daemon = Daemon(port=int(sys.argv[1]), database=sys.argv[2])
+    daemon = Daemon()#port=int(sys.argv[1]), database=sys.argv[2])
     daemon.run()
