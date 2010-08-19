fcbef1729ca2a0b5dc27a3a7c919a9bd7cd1f0bb
Krzysztof Rutka <rutka@student.agh.edu.pl>
2010-08-19 20:45:45 +0200

    Daemon config and stuff.

--- daemon/modules/Daemon.py
+++ daemon/modules/Daemon.py
@@ -13,34 +13,36 @@ from modules.Manager import Manager
 class Daemon:
     manager = Manager()
         
-    def __init__(self, port=4567, database='aretes.db', config='aretes.cfg'):
+    def __init__(self, host='localhost', port=4567, database='aretes.db', config='aretes.cfg'):
         self.config_filename = config
         self.database_filename = database
-        self._setup_database(database)
-        self._setup_config(config)
+        self._setup_database()
+        self._setup_config()
         
         from modules.Handler import Handler
         SocketServer.TCPServer.allow_reuse_address = True
-        self.tcp_server = SocketServer.TCPServer(('localhost', port), Handler)
+        self.tcp_server = SocketServer.TCPServer((host, port), Handler)
 
-    def _setup_database(self, database):
-        metadata.bind = 'sqlite:///' + database
+    def _setup_database(self):
+        metadata.bind = 'sqlite:///' + self.database_filename
         setup_all()
         create_all()
     
-    def _setup_config(self, config):
+    def _setup_config(self):
         config = ConfigParser.SafeConfigParser()
-        config.read(str(config))
+        config.read(self.config_filename)
     
         # Default settings:
         TMPDIR = './tmp'
+        DBFILE = self.database_filename
     
         try:
             tmpdir = config.get('AreteS', 'tmpdir')
         except ConfigParser.NoSectionError:
             config.add_section('AreteS')
             config.set('AreteS', 'tmpdir', TMPDIR)
-            with open(str(config), 'wb') as f:
+            config.set('AreteS', 'dbfile', DBFILE)
+            with open(self.config_filename, 'wb') as f:
                 config.write(f)
         finally:
             try:
@@ -52,8 +54,20 @@ class Daemon:
                     raise
     
     def run(self):
-        self.tcp_server.serve_forever()
+        print '[Arete Slave] Started @ %s:%d' % (self.tcp_server.server_address[0], self.tcp_server.server_address[1])
+        try:
+            self.tcp_server.serve_forever()
+        except KeyboardInterrupt:
+            print '\n[Arete Slave] Ended'
     
     @classmethod
     def get_manager(self):
         return self.manager
+    
+    @classmethod
+    def get_config_parameter(self, param):
+        config = ConfigParser.SafeConfigParser()
+        config.read(self.config_filename)
+        
+        return config.get('AreteS', param)
+        
