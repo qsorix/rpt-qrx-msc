1c6196b5f3ab17dd6709781aadf570c518112a03
Krzysztof Rutka <rutka@student.agh.edu.pl>
2010-08-19 20:47:09 +0200

    Handler exception handling and new multiple output sending.

--- daemon/modules/Handler.py
+++ daemon/modules/Handler.py
@@ -42,10 +42,10 @@ class Handler(SocketServer.StreamRequestHandler):
                     type, params = parse(line, parent)
                     if types_and_actions.get(type):
                         result = types_and_actions.get(type)(parent_id, **params)
-                except DaemonError as de:
-                    print >> sys.stderr, de
-                    if isinstance(de, CheckError):
-                        self.send_check_error()
+                except (DaemonError, CommandError) as e:
+                    print >> sys.stderr, e
+                    if isinstance(e, CommandError):
+                       self.send_cmd_error(e.cmd_id)
                     else:
                         self.send_bad_request()
                 else:
@@ -61,9 +61,16 @@ class Handler(SocketServer.StreamRequestHandler):
                         with open(path, 'wb') as f:
                             f.write(self.request.recv(size))
                     if type == 'results_get':
-                        output = result
-                        self.send_ok(size=len(output))
-                        self.wfile.write(output)
+                        if isinstance(result, tuple):
+                            number = result[0]
+                            output_list = result[1]
+                            size_list = [len(output) for output in output_list]
+                            self.send_ok(sizes=size_list)
+                            for output in output_list:
+                                self.wfile.write(output)
+                        else:
+                            self.send_ok(sizes=[len(result)])
+                            self.wfile.write(result)
                     else:
                         self.send_ok()
         except socket.error, IOError:
@@ -79,17 +86,18 @@ class Handler(SocketServer.StreamRequestHandler):
         print >> sys.stderr, "[%s] Sending: %s" % (self.client_address[0], msg)
         self.wfile.write(msg + '\n')
 
-    def send_ok(self, size=None):
+    def send_ok(self, sizes=None):
         msg = '200 OK'
-        if size:
-            msg += ' %d' % size
+        if sizes:
+            for size in sizes:
+                msg += ' %d' % size
         self.send(msg)
 
     def send_bad_request(self):
         self.send('400 Bad Request')
 
-    def send_check_error(self):
-        self.send('401 Check Error')
+    def send_cmd_error(self, cmd_id):
+        self.send('401 Command Failed: %s' % cmd_id)
 
     # TODO: send this if test end policy is set to 'complete' and scheduler has executed all tasks
     def send_test_finished(self):
