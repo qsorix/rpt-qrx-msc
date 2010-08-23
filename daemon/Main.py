e7895c31686fa2298afc99c4bf7a2c0a925afcdf
Krzysztof Rutka <rutka@student.agh.edu.pl>
2010-08-23 13:34:32 +0200

    Added command line arguments.

--- daemon/Main.py
+++ daemon/Main.py
@@ -2,10 +2,18 @@
 # coding=utf-8
 
 import sys
+import argparse
 
 from modules.Daemon import Daemon
 
 if __name__ == "__main__":
-    # TODO Add some command line parameters
-    daemon = Daemon()#port=int(sys.argv[1]), database=sys.argv[2])
+    parser = argparse.ArgumentParser(prog='Main.py')
+    
+    parser.add_argument('-p', '--port', help="use port other than 4567", required=False, nargs='?', type=int, default=4567)
+    parser.add_argument('-d', '--database', help="use database file other than 'aretes.db'", required=False, nargs='?', type=str, default='aretes.db')
+    parser.add_argument('-l', '--log', help="use log file other than 'aretes.log'", required=False, nargs='?', type=str, default='aretes.log')
+
+    args = parser.parse_args()
+    
+    daemon = Daemon(port=args.port, database=args.database, log=args.log)
     daemon.run()
