e71378a3da7f6f9e6dc66d997f72fe031aeb228b
Krzysztof Rutka <rutka@student.agh.edu.pl>
2010-08-24 15:49:27 +0200

    Command arguments fix.

--- daemon/Main.py
+++ daemon/Main.py
@@ -9,9 +9,9 @@ from modules.Daemon import Daemon
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='Main.py')
     
-    parser.add_argument('-p', '--port', help="use port other than 4567", required=False, nargs='?', type=int, default=4567)
-    parser.add_argument('-d', '--database', help="use database file other than 'aretes.db'", required=False, nargs='?', type=str, default='aretes.db')
-    parser.add_argument('-l', '--log', help="use log file other than 'aretes.log'", required=False, nargs='?', type=str, default='aretes.log')
+    parser.add_argument('-p', '--port', help="use port other than 4567", required=False, type=int, default=4567)
+    parser.add_argument('-d', '--database', help="use database file other than 'aretes.db'", required=False, type=str, default='aretes.db')
+    parser.add_argument('-l', '--log', help="use log file other than 'aretes.log'", required=False, type=str, default='aretes.log')
 
     args = parser.parse_args()
     
