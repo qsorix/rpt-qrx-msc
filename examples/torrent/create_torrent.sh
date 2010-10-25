f6b0881105d336b9dd58203baa79374d7378eac3
Krzysztof Rutka <rptmail@gmail.com>
2010-10-25 16:57:01 +0200

    Added script for creating torrent data and file.

---
+++ examples/torrent/create_torrent.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -f big-data-file ]; then
+    echo "Big data file already exists..."
+else
+    echo "Creating 524 MB data file... (it will take some time)"
+    dd if=/dev/urandom of=./big-data-file bs=1M count=500
+fi
+
+type -P ctorrent &>/dev/null || { echo "Cannot create torrent, You don't have ctorrent :/" >&2; exit 1; }
+
+if [ -f big-data-file.torrent ]; then
+    rm big-data-file.torrent
+fi
+
+echo "Creating torrent file..."
+ctorrent -t -s ./big-data-file.torrent -u http://localhost:9876/announce -p ./big-data-file
+# FIXME Tutaj powinien byc adres trackera z testu...
