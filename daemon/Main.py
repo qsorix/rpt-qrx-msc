9d36a22f25c7146ca49b7719de66a7062594f7d6
Tomasz Rydzynski <qsorix@rydznet.pl>
2010-11-04 00:09:10 +0100

    basic SSH support in slave

--- daemon/Main.py
+++ daemon/Main.py
@@ -4,7 +4,27 @@
 import os
 import argparse
 import shutil
+import sys
 from modules.Daemon import Daemon
+
+try:
+    import paramiko
+    import base64
+    has_ssh_support = True
+except:
+    has_ssh_support = False
+
+def load_authorized_keys(filename):
+    #FIXME: ssh sprawdza prawa i odmowia pracy, jesli plik authorized_keys ma
+    # inne prawa niz 0400. To dobry pomysl jest :)
+    #
+    # poza tym wypadaloby moze te funkcje troche bardziej 'bledo-odpornie'
+    # napisac, teraz sie pewnie ostro sypnie jak w pliku beda jakies krzaki
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+
+        # wez druga kolumne z wszystkich linii, zdedokuj i zrob z niej klucze
+        return map(lambda x: paramiko.RSAKey(data=base64.decodestring(x.split()[1])), lines)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='Main.py')
@@ -15,6 +35,9 @@ if __name__ == "__main__":
     parser.add_argument('-v', '--verbose', help="print everything to stderr too", required=False, action='store_true')
     parser.add_argument('-n', '--new', help="delete current database and log", required=False, action='store_true')
     parser.add_argument('-c', '--clean', help="delete temporary file directory", required=False, action='store_true')
+    parser.add_argument('-s', '--ssh', help="use ssh to authorize connections", required=False, action='store_true', default=False)
+    parser.add_argument('--authorized-keys', help="path to authorized keys file", required=False, type=str)
+    parser.add_argument('--host-key', help="path to host's private key file", required=False, type=str)
 
     args = parser.parse_args()
  
@@ -27,5 +50,20 @@ if __name__ == "__main__":
         if os.path.isfile(args.log):
             os.remove(args.log)
 
-    daemon = Daemon(port=args.port, database=args.database, log=args.log, verbose=args.verbose)
+    if args.ssh:
+        if (not has_ssh_support):
+            print >> sys.stderr, "In order to use ssh you must install paramiko library."
+            sys.exit(1)
+
+        if (args.authorized_keys is None) or (args.host_key is None):
+            print >> sys.stderr, "In order to use ssh you must provide --authorized-keys and --host-key arguments"
+            sys.exit(1)
+
+        host_key = paramiko.RSAKey(filename=args.host_key)
+        authorized_keys = load_authorized_keys(args.authorized_keys)
+        daemon = Daemon(port=args.port, database=args.database, log=args.log, verbose=args.verbose, ssh=True, authorized_keys=authorized_keys, host_key=host_key)
+
+    else:
+        daemon = Daemon(port=args.port, database=args.database, log=args.log, verbose=args.verbose, ssh=False)
+
     daemon.run()
