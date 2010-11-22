2756fefeb39e3e0874f98cfac58fac30001e4c09
Krzysztof Rutka <rptmail@gmail.com>
2010-11-22 18:47:18 +0100

    Fix.

--- setup.py
+++ setup.py
@@ -5,9 +5,11 @@ setup(
     version = '1.0',
     description = 'Network testing tool',
     author = 'Krzysztof Rutka & Tomasz Rydzynski',
-    packages = ['AreteMaster', 'AreteMaster/command', 'AreteMaster/common', 'AreteMaster/config', 'AreteMaster/controller', 'AreteMaster/utils',
-        'AreteMaster/plugins', 'AreteMaster/plugins/connections', 'AreteMaster/plugins/drivers', 'AreteMaster/plugins/frontends',
+    packages = ['AreteMaster', 'AreteMaster/command', 'AreteMaster/common', 
+        'AreteMaster/config', 'AreteMaster/controller', 'AreteMaster/utils',
+        'AreteMaster/plugins', 'AreteMaster/plugins/connections',
+        'AreteMaster/plugins/drivers', 'AreteMaster/plugins/frontends',
         'AreteSlave', 'AreteSlave/modules', 'AreteSlave/common', 'AreteSlave/database'],
-    scripts = ['arete', 'arete-slave', 'arete-poker']
+    scripts = ['arete', 'arete-slave', 'arete-poker'],
 )
 
